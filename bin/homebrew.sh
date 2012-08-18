367f39c38ea4978c1c1542ac9a75bf2ee263ee0d
--global <pagefold@gmail.com>
2012-08-18 01:36:58 -0500

    Moved homebrew to brew

--- bin/homebrew.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-CMD="brew"
-$CMD -v >/dev/null
-if [ $? -ne 0 ] ; then
-  printf "$RED%-${COLWIDTH}s [installing]$RESET\n" $CMD
-else
-  printf "$GREEN%-${COLWIDTH}s [ok]$RESET\n" $CMD
-fi
